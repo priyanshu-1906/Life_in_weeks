666d56eac303f46b432fdc16408373a05b7d91bc
priyanshu-1906 <89680386+priyanshu-1906@users.noreply.github.com>
2022-07-19 00:58:07 +0530

    Update Life_in_weeks.py

--- Life_in_weeks.py
+++ Life_in_weeks.py
@@ -7,4 +7,7 @@ b = a*365
 c = a*52
 d = a*12
 print("You have "+ str(b) + " days, "+ str(c) + " weeks, and "+str(d)+ " months left."  )
+# or rather than printing like above you can rather use f-strings
+print(f"you have {b} days, {c} weeks, and {d} months left.")
+
 
